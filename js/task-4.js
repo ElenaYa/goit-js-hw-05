d82e6c874ff85d8c13ab2cd133a6a2fdf99f0fa4
ElenaYa <elena.yatsynyuk@gmail.com>
2023-11-19 00:45:17 +0200

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,11 +1,10 @@
-
 const getTotalBalanceByGender = (users, gender) => {
     return users.filter((user) => user.gender === gender)
         .reduce((acc, user) => {
         return acc + user.balance;
     }, 0);
 };
-const allUsers = [
+const allUsers1 = [
     {
     name: "Moore Hensley",
     gender: "male",
@@ -44,6 +43,6 @@ const allUsers = [
 ];
 
 
-console.log(getTotalBalanceByGender(allUsers, "male")); // 12053
+console.log(getTotalBalanceByGender(allUsers1, "male")); // 12053
 
-console.log(getTotalBalanceByGender(allUsers, "female")); // 8863
+console.log(getTotalBalanceByGender(allUsers1, "female")); // 8863
